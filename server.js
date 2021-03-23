e8a456d936309c38a3c920af68395824b5d84073
mloza97 <hector19865@hotmail.com>
2021-03-22 19:09:29 -0600

    Conf to deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(precess.env.PORT || 8080);
+app.listen(precess.env.PORT || 4200, function(){
+  console.log('Console listening port 4200!');
+});
 
 //PathLocationStrategy
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
-
-console.log('Console listening!');
