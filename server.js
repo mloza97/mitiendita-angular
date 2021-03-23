da0beac4686fd1768dc8887a2b601d5bc80930e5
mloza97 <hector19865@hotmail.com>
2021-03-23 08:28:18 -0600

    Se cambio la configuracion para  el deploy

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(precess.env.PORT || 4200, function(){
-  console.log('Console listening port 4200!');
-});
-
 //PathLocationStrategy
-app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/mitiendita-app'}),
+);
+
+app.listen(process.env.PORT || 8080);
