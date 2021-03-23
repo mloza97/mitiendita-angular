10dd09a32a8196c796f43082e7521dc2baeec5c4
mloza97 <hector19865@hotmail.com>
2021-03-23 08:45:02 -0600

    Se cambio la configuracion para  el deploy

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 //PathLocationStrategy
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/'}),
-);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log('Console listening port 8080!');
+});
